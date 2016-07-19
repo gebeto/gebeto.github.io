e11cb27c9a9e4cdf0052f9c11da012ffde509cd4
slavik.nychkalo <bboyheadman@gmail.com>
2016-07-19 11:57:53 +0300

    Added new site!

---
+++ assets/js/html5shiv.js
@@ -0,0 +1,67 @@
+/* 
+   HTML5 Shiv v3.6.2 | @afarkas @jdalton @jon_neal @rem | MIT/GPL2 Licensed
+ */
+function(l, f) {
+    function m() {
+        var ae.elementsreturn "string"
+        typeof aa.split(" ": a
+        }
+
+        function i(a) {
+            var bn[a[o]] bb {}, h++, a[o] h, n[h] b) return b
+    }
+
+    function p(a, b, c) {
+        bbf) if (g) return b.createElement(a) cci(b)) bc.cache[a] c.cache[a].cloneNode(: r.test(a) c.cache[a] c.createElem(a)).cloneNode(: c.createElem(a) return b.canHaveChildrens.test(a) c.frag.appendChild(b: b
+    }
+
+    function t(a, b) {
+        if (b.cache) b.cache {}, b.createElema.createElement, b.createFraga.createDocumentFragment, b.fragb.createFrag() a.createElementfunction(c) {
+            returne.shivMethodsb.createElem(c: p(c, a, b)
+            }
+            a.createDocumentFragmentFunction("h, f", "return function(){var n=f.cloneNode(),c=n.createElement;h.shivMethods&&(" + m().join().replace(w + g, function(a) {
+                    b.createElem(a) b.frag.createElement(a) return 'c("' + a + '")'
+                } + ")return n}")(e, b.frag)
+            }
+
+            function q(a) {
+                a || (a = f);
+                var b = i(a);
+                if (e.shivCSS && !j && !b.hasCSS) {
+                    var c, d = a;
+                    c = d.createElement("p") dd.getElementsByTagName("head")[0] d.documentElementc.innerHTML "x<style>article,aside,figcaption,figure,footer,header,hgroup,main,nav,section{display:block}mark{background:#FF0;color:#000}</style>"
+                    cd.insertBefore(c.lastChild, d.firstChild) b.hasCSSc
+                }
+                gt(a, b) return a
+            }
+            var kl.html5 {}, s: buttonmapselecttextareaobjectiframeoptionoptgroup) i, r: abcodedivfieldseth1h2h3h4h5h6ilabelliolpqspanstrongstyletabletbodytdthtrul) i, j, o "_html5shiv", h0, n {}, gfunction() {
+        try {
+            var af.createElement("a") a.innerHTML "<xyz></xyz>"
+            j "hidden" in avar bif(b1a.childNodes.length)) {
+            f.createElement("a") var cf.createDocumentFragment() b "undefined"
+            typeof c.cloneNode "undefined"
+            typeof c.createDocumentFragment "undefined"
+            typeof c.createElement
+        }
+        gb
+    } catch (d) {
+        gj0
+    }
+}
+var e {
+    elements: k.elements "abbr article aside audio bdi canvas data datalist details figcaption figure footer header hgroup main mark meter nav output progress section summary time video",
+    version: "3.6.2",
+    shivCSS: 1k.shivCSS,
+    supportsUnknownElements: g,
+    shivMethods: 1k.shivMethods,
+    type: "default",
+    shivDocument: q,
+    createElement: p,
+    createDocumentFragment: function(a, b) {
+        aaf) if (g) return a.createDocumentFragment() for (var bbi(a), cb.frag.cloneNode(), d0, em(), he.lengthdhd++) c.createElement(e[d]
+        return c
+    }
+}
+l.html5eq(f)
+}
+this, document)
