883d2faa0655b2a03cd3f1d1fa85506ff7a75bfc
bboyheadman <bboyheadman@gmail.com>
2016-08-12 16:27:56 +0300

    'Update'

--- repo/PACK.py
+++ repo/PACK.py
@@ -1,5 +1,10 @@
-import bz2
+import bz2, os, time
+
 Packages = open("Packages").readlines()
 print Packages
 Packagesbz = bz2.BZ2File("Packages.bz2", "w")
 Packagesbz.writelines(Packages)
+
+os.system("git add -A")
+os.system("git commit -m 'Update'")
+os.system("git push")
