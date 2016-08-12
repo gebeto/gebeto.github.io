daf4f4c9e5861c33bdbb440f72dfc5b1a08eb4f6
bboyheadman <bboyheadman@gmail.com>
2016-08-12 16:38:19 +0300

    Something

--- repo/UpdateRepo.py
+++
@@ -1,9 +0,0 @@
-import bz2, os, time
-
-Packages = open("Packages").readlines()
-Packagesbz = bz2.BZ2File("Packages.bz2", "w")
-Packagesbz.writelines(Packages)
-
-os.system("git add -A")
-os.system("git commit -m 'Update'")
-os.system("git push")
