634c2f6f52fa5e6b404e1bed95b55a3cd0b22f60
slavik.nychkalo <bboyheadman@gmail.com>
2016-10-10 17:28:21 +0300

    **IT IS QUICK COMMIT AND PUSH**

--- narko/main.js
+++ narko/main.js
@@ -1,30 +1,36 @@
 function changeText(elem) {
-	var text = prompt("Enter text");
-	if (text.length > 0)
+	var text = prompt('Enter text');
+	if (text.length > 0) {
 		elem.innerHTML = text;
+	}
 }
 
 function load() {
-	
-	// document.getElementsByClassName("wrapper")[0].setAttribute("style", "background:"+getRandomColor());
-	document.getElementsByClassName("cube1")[0].setAttribute("style", "background:"+getRandomColor());
-	document.getElementsByClassName("cube2")[0].setAttribute("style", "background:"+getRandomColor());
-	document.getElementsByClassName("first")[0].setAttribute("style", "background:"+getRandomColor());
-	document.getElementsByClassName("second")[0].setAttribute("style", "background:"+getRandomColor());
-	document.getElementsByClassName("third")[0].setAttribute("style", "background:"+getRandomColor());
-	document.getElementById("section1").setAttribute("style", "background:"+getRandomColor());
-	document.getElementById("section2").setAttribute("style", "background:"+getRandomColor());
-	document.getElementById("section3").setAttribute("style", "background:"+getRandomColor());
-	document.getElementById("section4").setAttribute("style", "background:"+getRandomColor());
+	// document.getElementsByClassName('wrapper')[0].setAttribute('style', 'background:'+getRandomColor());
+	document.getElementsByClassName('cube1')[0].setAttribute('style', 'background:'+getRandomColor());
+	document.getElementsByClassName('cube2')[0].setAttribute('style', 'background:'+getRandomColor());
+	document.getElementsByClassName('first')[0].setAttribute('style', 'background:'+getRandomColor());
+	document.getElementsByClassName('second')[0].setAttribute('style', 'background:'+getRandomColor());
+	document.getElementsByClassName('third')[0].setAttribute('style', 'background:'+getRandomColor());
+	document.getElementById('section1').setAttribute('style', 'background:'+getRandomColor());
+	document.getElementById('section2').setAttribute('style', 'background:'+getRandomColor());
+	document.getElementById('section3').setAttribute('style', 'background:'+getRandomColor());
+	document.getElementById('section4').setAttribute('style', 'background:'+getRandomColor());
 	setTimeout(function() { load() }, 10);
 }
 
-function rC() {
-	var ran = Math.round(Math.random()*15);
-	console.log(ran);
-	return ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"][ran];
-}
+// OLD VERSION
+// function rC() {
+// 	var ran = Math.round(Math.random()*15);
+// 	console.log(ran);
+// 	return ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'][ran];
+// }
 
 function getRandomColor() {
-	return "#"+rC()+rC()+rC()+rC()+rC()+rC();
+	var randColor = '#';
+	for (var i = 0; i < 6; i++) {
+		randColor += Math.round(Math.random()*15).toString(16);
+	}
+	return randColor;
 }
+
