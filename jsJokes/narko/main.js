bcb32056945cfe23e5ce32ab3c7d89b994210230
slavik.nychkalo <bboyheadman@gmail.com>
2016-10-22 21:38:15 +0300

    **IT IS QUICK COMMIT AND PUSH**

---
+++ jsJokes/narko/main.js
@@ -0,0 +1,41 @@
+function spliceOnParts(word) {
+	var result = [];
+	result.push(word.slice(0, word.length/3));
+	result.push(word.slice(word.length/3, word.length/3*2));
+	result.push(word.slice(word.length/3*2, word.length));
+	return result;
+}
+
+function changeText(elem) {
+	var text = prompt('Enter text');
+	if (text.length > 0) {
+		elem.innerHTML = text;
+	}
+}
+
+function load() {
+	var elements = [
+		document.getElementsByClassName('cube1')[0],
+		document.getElementsByClassName('cube2')[0],
+		document.getElementsByClassName('first')[0],
+		document.getElementsByClassName('second')[0],
+		document.getElementsByClassName('third')[0],
+		document.getElementById('section1'),
+		document.getElementById('section2'),
+		document.getElementById('section3'),
+		document.getElementById('section4')
+	];
+	setInterval(function() { 
+		elements.forEach(function(element){
+			element.setAttribute('style', 'background:'+getRandomColor());
+		});
+	}, 20);
+}
+
+function getRandomColor() {
+	var randColor = '#';
+	for (var i = 0; i < 6; i++) {
+		randColor += Math.round(Math.random()*15).toString(16);
+	}
+	return randColor;
+}
