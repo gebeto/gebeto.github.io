402a9d69fb74ca9ab3e5e9e2e514e5e380385535
slavik.nychkalo <bboyheadman@gmail.com>
2016-10-23 22:02:53 +0300

    **IT IS QUICK COMMIT AND PUSH**

---
+++ jsJokes/paterns/main.js
@@ -0,0 +1,24 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+var w = window.innerWidth;
+var h = window.innerHeight;
+
+canvas.height = h;
+canvas.width = w;
+
+var stroke = 'Hello world!';
+var cnt = 0;
+
+	ctx.translate(w/2, h/2)
+function mainLoop() {
+	
+//	ctx.clearRect(0, 0, w, h);
+	ctx.fillText(stroke.slice(0, cnt), 0, 0);
+	cnt += 1;
+	ctx.rotate(0.1);
+	setTimeout(mainLoop, Math.random()*100+100);
+	
+}
+
+mainLoop();
