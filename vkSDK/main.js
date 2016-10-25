77132ca9d6499099fcc6c518b221462239f329f1
slavik.nychkalo <bboyheadman@gmail.com>
2016-10-25 21:59:10 +0300

    **IT IS QUICK COMMIT AND PUSH**

---
+++ vkSDK/main.js
@@ -0,0 +1,10 @@
+VK.init(function() {
+		console.log('success');
+		// API initialization succeeded
+     	// Your code here 
+	}, function() {
+		console.log('error');
+		// API initialization failed
+     	// Can reload page here 
+}, '5.59');
+
