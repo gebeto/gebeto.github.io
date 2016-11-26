565edc1e107791eda25d29059f3b049cb75b8cde
slavik.nychkalo <bboyheadman@gmail.com>
2016-11-26 20:40:51 +0200

    **IT IS QUICK COMMIT AND PUSH**

--- jsProjects/livePaint/main.js
+++ jsProjects/livePaint/main.js
@@ -3,7 +3,7 @@ var title = document.getElementById('title');
 var db = firebase.database().ref().child('posts');
 db.on('value', function(posts) {
 	// title.innerText = text.val();
-	console.log(posts);
+	console.log(posts.val());
 });
 
 document.getElementById('submit').addEventListener('click', function(){
