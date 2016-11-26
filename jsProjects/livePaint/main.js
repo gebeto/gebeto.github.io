70e9f3961d671e078d938598320e0d4e9f3904f5
slavik.nychkalo <bboyheadman@gmail.com>
2016-11-26 20:18:53 +0200

    **IT IS QUICK COMMIT AND PUSH**

--- jsProjects/livePaint/main.js
+++ jsProjects/livePaint/main.js
@@ -1,5 +1,6 @@
 var title = document.getElementById('title');
-var db = firebase.database().ref().child('text');
+var db = firebase.database().ref().child('title');
 db.on('value', function(text) {
 	title.innerText = text.val();
+	console.log(text);
 });
