c2b480f8d3c7a4bd8203cdb364ed1ac4728657dc
slavik.nychkalo <bboyheadman@gmail.com>
2016-11-26 20:44:23 +0200

    **IT IS QUICK COMMIT AND PUSH**

--- jsProjects/livePaint/main.js
+++ jsProjects/livePaint/main.js
@@ -1,13 +1,26 @@
-var title = document.getElementById('title');
+(function(){
+		
+	var config = {
+		apiKey: "AIzaSyAwKMoKj_sk6TQV-paWdgarGW8w8v8XQ1Q",
+		authDomain: "nulp-93482.firebaseapp.com",
+		databaseURL: "https://nulp-93482.firebaseio.com",
+		storageBucket: "nulp-93482.appspot.com",
+		messagingSenderId: "163026386106"
+	  };
+	firebase.initializeApp(config);
 
-var db = firebase.database().ref().child('posts');
-db.on('value', function(posts) {
-	// title.innerText = text.val();
-	console.log(posts.val());
-});
+	var title = document.getElementById('title');
 
-document.getElementById('submit').addEventListener('click', function(){
+	var db = firebase.database().ref().child('posts');
+	db.on('value', function(posts) {
+		// title.innerText = text.val();
+		console.log(posts.val());
+	});
 
-	var input = document.getElementById('textInput');
+	document.getElementById('submit').addEventListener('click', function(){
 
-});
+		var input = document.getElementById('textInput');
+
+	});
+
+}());
