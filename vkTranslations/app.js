680c48bd465efddbf9bda57320816687166e1673
slavik.nychkalo <bboyheadman@gmail.com>
2016-12-26 21:13:39 +0200

    **IT IS QUICK COMMIT AND PUSH**

---
+++ vkTranslations/app.js
@@ -0,0 +1,3 @@
+var url = "https://api.vk.com/method/video.getRecommendedLiveVideos?access_token=c294207da18b7846e6172f8e0fc103cf4ec432a2c669c216cbd0d0f43530c70b71a945311f66ada1a20c5&count=50&extended=1&fields=photo_50,photo_100,photo_200_orig,photo_200,photo_400_orig,photo_max,photo_max_orig,screen_name,city,country,friend_status,counters,first_name_gen,sex,is_video_live_notifications_blocked,blacklisted,blacklisted_by_me,bdate,followers_count,members_count,member_status,city,country,is_video_live_notifications_blocked&https=1&lang=en&offset=0&v=5.60";
+
+
