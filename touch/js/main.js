d4948ec0748013f964866ed2d5fad8c1b81eec2b
bboyheadman <bboyheadman@gmail.com>
2017-02-14 22:44:23 +0200

    QuickCommit

--- touch/js/main.js
+++ touch/js/main.js
@@ -3,9 +3,9 @@ $(function() {
     //Generic swipe handler for all directions
     swipeStatus: function(event, action, direction, duration, arg, arg1) {
       console.log('cell swipe', action, direction, arg);
-      if (direction === 'left' && arg > 150) {
+      if (direction === 'left' && arg > 120) {
         $(this).addClass('opened');
-      } else if (direction === 'right' && arg > 150) {
+      } else if (direction === 'right' && arg > 120) {
         $(this).removeClass('opened');
       }
     }
