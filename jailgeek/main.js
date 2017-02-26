8ae40c4e61c0cca8af7bc7d710437818dbad5b08
bboyheadman <bboyheadman@gmail.com>
2017-02-26 17:51:43 +0200

    QuickCommit

--- jailgeek/main.js
+++ jailgeek/main.js
@@ -1,10 +1,41 @@
-var mainTitle = document.getElementById('main-text');
 var canvas = document.getElementById('main-canvas');
-var ctx = canvas.getContext('2d');
-var font = {
-	size: 150,
-	family: 'CoreSansDS67CnHeavyItalic',
+
+function CanvasDrawer(canvas, bgUrl) {
+	this.canvas = canvas;
+	this.ctx = this.canvas.getContext('2d');
+	this.size = 150;
+	this.family = 'CoreSansDS67CnHeavyItalic';
+	this.background = this.loadImage(bgUrl);
 }
+
+CanvasDrawer.prototype.loadImage = function(iurl) {
+	var bg = new Image();
+	bg.addEventListener('load', function() {
+		this.ctx.drawImage(bg, 0, 0);
+	}.bind(this));
+	bg.src = iurl;
+	return bg;
+}
+
+CanvasDrawer.prototype.setFillStyle = function() {
+	var gradient = this.ctx.createLinearGradient(this.canvas.width/2, this.canvas.height/3, this.canvas.width/2, this.canvas.height - this.canvas.height/3);
+	gradient.addColorStop(0.0, "#FCFDFF");
+	gradient.addColorStop(1.0, "#E5F4FA");
+	this.ctx.fillStyle = gradient;
+	this.ctx.font = this.size + 'px ' + this.family;
+	this.ctx.textAlign = 'center';
+}
+
+CanvasDrawer.prototype.setup = function() {
+	this.setFillStyle();
+}
+
+CanvasDrawer.prototype.refreshTitle = function(text) {
+	this.ctx.drawImage(this.background, 0, 0);
+	this.ctx.fillText(text, 425, 280 + this.size / 3);
+}
+
+var drawer = new CanvasDrawer(canvas, 'background.svg');
 
 WebFontConfig = {
 	custom: {
@@ -12,29 +43,12 @@ WebFontConfig = {
 		urls: ['fonts.css']
 	},
 	active: function() {
-		var bg = loadImage('vkpass.svg');
-		ctx.font = font.size + 'px ' + font.family;
-		ctx.textAlign = 'center';
-
-		var gradient = ctx.createLinearGradient(canvas.width/2, canvas.height/3, canvas.width/2, canvas.height - canvas.height/3);
-		gradient.addColorStop(0.0, "#FCFDFF");
-		gradient.addColorStop(1.0, "#E5F4FA");
-		ctx.fillStyle = gradient;
 		document.getElementById('text-input').addEventListener('keyup', function (e) {
-			ctx.drawImage(bg, 0, 0);
-			ctx.fillText(this.value, 425, 280 + font.size / 3);
+			// CanvasDrawer.refreshTitle(this.value);
+			// drawer.
 		});
 	}
 };
-
-function loadImage(iurl) {
-	var bg = new Image();
-	bg.addEventListener('load', function() {
-		ctx.drawImage(bg, 0, 0);
-	});
-	bg.src = iurl;
-	return bg;
-}
 
 
 
