266e334deeb0a0db9cd8ca7d799e69f1cc0dcf2b
gebeto <bboyheadman@gmail.com>
2017-04-22 05:01:28 +0300

    QuickCommit

--- jailgeek/module/assets/main.js
+++ jailgeek/module/assets/main.js
@@ -33,8 +33,6 @@ CanvasDrawer.prototype.loadImage = function(iurl) {
 		this.ctx.drawImage(bg, 0, 0);
 	}.bind(this));
 	bg.src = iurl;
-	
-	img.src = this.canvas.toDataURL();
 	return bg;
 }
 
@@ -45,8 +43,6 @@ CanvasDrawer.prototype.setFillStyle = function() {
 	this.ctx.fillStyle = gradient;
 	this.ctx.font = this.fontSize + 'px ' + this.fontFamily;
 	this.ctx.textAlign = 'center';
-	
-	img.src = this.canvas.toDataURL();
 }
 
 CanvasDrawer.prototype.refreshTitle = function(text) {
@@ -58,29 +54,21 @@ CanvasDrawer.prototype.refreshTitle = function(text) {
 		var top = this.top + this.fontSize / 3;
 		this.ctx.fillText(line, 425, top + this.lineSpacing * i);
 	}.bind(this));
-	
-	img.src = this.canvas.toDataURL();
 }
 
 CanvasDrawer.prototype.moveOneLineUp = function() {
 	this.top -= this.fontSize / 2;
 	this.refreshTitle();
-	
-	img.src = this.canvas.toDataURL();
 }
 
 CanvasDrawer.prototype.moveOneLineDown = function() {
 	this.top += this.fontSize / 2;
 	this.refreshTitle();
-	
-	img.src = this.canvas.toDataURL();
 }
 
 CanvasDrawer.prototype.moveToCenter = function() {
 	this.top = this.canvas.height / 2 - this.fontSize / 2 * (this.currentText.split('\n').length - 1);
 	this.refreshTitle();
-	
-	img.src = this.canvas.toDataURL();
 }
 
 CanvasDrawer.prototype.changeFontSize = function(size) {
@@ -88,15 +76,11 @@ CanvasDrawer.prototype.changeFontSize = function(size) {
 	this.fontSize = size;
 	this.ctx.font = this.fontSize + 'px ' + this.fontFamily;
 	this.refreshTitle();
-	
-	img.src = this.canvas.toDataURL();
 }
 
 CanvasDrawer.prototype.changeLineSpacing = function(size) {
 	this.lineSpacing = size;
 	this.refreshTitle();
-	
-	img.src = this.canvas.toDataURL();
 }
 
 var drawer = new CanvasDrawer(canvas, 'assets/background.svg');
