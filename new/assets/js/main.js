1fc9075739ad6191e666caac0db52079248ff5cc
gebeto <bboyheadman@gmail.com>
2017-08-09 22:42:05 +0300

    newwest

--- new/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-;(function(){
-	document.querySelector('.toggle').addEventListener('click', function(e) {
-		if (document.body.className === 'fullscreen') {
-			document.body.className = '';
-		} else {
-			document.body.className = 'fullscreen';
-		}
-	});
-}())
