536eef66415fd87abf67d95f3f1f0776e4cdc067
gebeto <bboyheadman@gmail.com>
2017-08-09 23:58:00 +0300

    almost don

---
+++ new/assets/js/main.js
@@ -0,0 +1,30 @@
+var POSTS_WRAPPER = document.querySelector('.mini-posts');
+var POSTS = [{
+  title: 'Theos Projects',
+  image: 'theos-projects.png',
+},{
+  title: 'VK Graffiti Uploader',
+  image: 'graffiti-uploader.png',
+},];
+
+function MiniPost(data) {
+  // Data
+  //  Title
+  //  Time
+  //  Author
+  //  Image
+  var html = ' <header>';
+  html += '       <h3><a href="#">' + data.title + '</a></h3>';
+  html += '       <time class="published" datetime="2015-10-20">October 20, 2015</time>';
+  html += '       <a href="#" class="author"><img src="images/avatar.jpg" alt="" /></a>';
+  html += '     </header>';
+  html += '     <a href="#" class="image"><img src="images/' + data.image + '" alt="" /></a>';
+  var elem = document.createElement('article');
+  elem.className = 'mini-post';
+  elem.innerHTML = html;
+  return elem;
+}
+
+POSTS.map(function(post, index) {
+  POSTS_WRAPPER.appendChild(new MiniPost(post))
+});
