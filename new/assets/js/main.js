8fbf0cd01f5575f099fab3ec0a6b690a8d642b47
gebeto <bboyheadman@gmail.com>
2017-08-11 18:08:06 +0300

    update

--- new/assets/js/main.js
+++ new/assets/js/main.js
@@ -1,85 +1,38 @@
+// October 20, 2015
+var DAYS = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+var MONTHS = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
 var GITHUB_REPOS_URL = 'https://api.github.com/users/gebeto/repos';
 function getAllRepos() {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', GITHUB_REPOS_URL);
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4) {
-      console.log(JSON.parse(xhr.responseText));
+      var resp = JSON.parse(xhr.responseText)
+      console.log(resp);
+      createPosts(resp);
     }
   }
   xhr.send();
 }
 
 
-var POSTS_WRAPPER = document.querySelector('.mini-posts');
-var POSTS = [
-{
-  title: 'Theos Projects',
-  image: 'theos-projects.png',
-},
-{
-  title: 'VK Graffiti Uploader',
-  image: 'graffiti-uploader.png',
-},
-{
-  title: 'Google Chrome plugins',
-  image: 'chrome-plugins.png',
-},
-{
-  title: 'Howdy Ho Sublime Plugin',
-  image: 'howdy-ho-post-sublime.png',
-},
-{
-  title: 'learn.javascript.ru offline',
-  image: 'learnjsru-reactnative-app.png',
-},
-{
-  title: 'LinkedId Downloader',
-  image: 'linkedin-learning-downloader.png',
-},
-{
-  title: 'Mini Web Projects',
-  image: 'mini-web-projects.png',
-},
-{
-  title: 'Treehouse downloader',
-  image: 'python-treehouse-downloader.png',
-},
-{
-  title: 'ReactJS Emoji',
-  image: 'react-emoji.png',
-},
-{
-  title: 'React Native Docs iOS',
-  image: 'react-native-docs.png',
-},
-{
-  title: 'VK Audio Message',
-  image: 'react-native-vk-audio-messages.png',
-},
-{
-  title: 'Sublime Auto Inputer',
-  image: 'sublime-auto-inputer.png',
-},
-{
-  title: 'VK Multi Bot',
-  image: 'vk-multi-bot.png',
-},
-{
-  title: 'VK Voice Changer',
-  image: 'vk-voice-changer.png',
-},
-];
+function createPosts(posts) {
+  posts.map(function(post, index) {
+    console.log(post);
+    var minipost = new MiniGithubPost(post);
+    POSTS_WRAPPER.appendChild(minipost);
+  });
+}
 
-function MiniPost(data) {
-  // Data
-  //  Title
-  //  Time
-  //  Author
-  //  Image
+
+var POSTS_WRAPPER = document.querySelector('.mini-posts');
+
+function MiniGithubPost(data) {
+	var date = new Date(data.pushed_at);
   var html = ' <header>';
-  html += '       <h3><a href="#">' + data.title + '</a></h3>';
-  html += '       <time class="published" datetime="2015-10-20">October 20, 2015</time>';
+  html += '       <h3><a href="#">' + data.name + '</a></h3>';
+  html += '       <time class="published" datetime="2015-10-20">' + MONTHS[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear() + '</time>';
   html += '       <a href="#" class="author"><img src="images/avatar.jpg" alt="" /></a>';
   html += '     </header>';
   // html += '     <a href="#" class="image"><img src="images/' + data.image + '" alt="" /></a>';
@@ -88,18 +41,4 @@ function MiniPost(data) {
   elem.innerHTML = html;
   return elem;
 }
-
-POSTS.map(function(post, index) {
-  var minipost = new MiniPost(post);
-  minipost.addEventListener('click', function(e) {
-    if (this.className === 'mini-post') {
-      this.className = 'mini-post opened';
-    } else if (this.className === 'mini-post opened') {
-      this.className = 'mini-post';
-    }
-    e.preventDefault();
-    e.stopPropagation();
-  });
-  POSTS_WRAPPER.appendChild(minipost);
-});
 
