5fd796872f31c2332a25da143673b755e94b2ca0
gebeto <slavik.nychkalo@gmail.com>
2018-03-16 00:21:55 +0200

    upd repo

---
+++ repo/unpack.sh
@@ -0,0 +1,6 @@
+mkdir -p pkgs
+debs=`ls debs/*.deb | xargs -n 1 basename`
+for entry in $debs
+	do echo "$entry";
+	`dpkg-deb -R debs/$entry pkgs/${entry%.deb}`;
+done
--- repo/update.sh
+++ repo/update.sh
@@ -1,3 +1,6 @@
+# ./unpack.sh
+# read -p "Change packages if need and press enter: "
+./pack.sh
 dpkg-scanpackages -m . /dev/null > Packages
 bzip2 -c Packages > Packages.bz2
 gzip  -c Packages > Packages.gz
