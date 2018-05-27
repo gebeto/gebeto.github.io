3caa7150004044bdcaf7a3958ff5019bf6e1c5a1
gebeto <slavik.nychkalo@gmail.com>
2018-05-27 18:31:32 +0300

    upd

---
+++ for-fun/akella-lerpy/index.d.ts
@@ -0,0 +1,9 @@
+declare module "*.glsl" {
+	const value: string;
+	export default value;
+}
+
+// declare module "three-orbit-controls" {
+// 	const value: (param: any) => any;
+// 	export default value;
+// }
