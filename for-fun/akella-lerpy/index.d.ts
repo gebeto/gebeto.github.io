5a52aad59ee4893732acf8d745f1932311d7c49a
gebeto <slavik.nychkalo@gmail.com>
2018-10-22 11:33:28 +0300

    move projects to separate repos

--- for-fun/akella-lerpy/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.glsl" {
-	const value: string;
-	export default value;
-}
-
-// declare module "three-orbit-controls" {
-// 	const value: (param: any) => any;
-// 	export default value;
-// }
