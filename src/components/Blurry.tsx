19828381338775c8eb009155673562061384ea2c
gebeto <slavik.nychkalo@gmail.com>
2022-05-02 01:18:30 +0300

    optimize gradients

--- src/components/Blurry.tsx
+++ src/components/Blurry.tsx
@@ -1,15 +1,19 @@
 import styled, { css } from "styled-components"
 import { detect } from "detect-browser"
 
+import blurryUa from "./Blurry-ua.png"
+import aaa from "./aaa.png"
+
 const browser = detect()
 
 const blurryColors = {
   ukraine: css`
-    background-image: linear-gradient(
+    /* background-image: linear-gradient(
       to bottom right,
       rgba(41, 128, 185, 0.4) 42%,
       rgba(181, 147, 8, 0.4) 58%
-    );
+    ); */
+    background-image: url(${blurryUa});
   `,
   purple: css`
     background-image: linear-gradient(
@@ -20,50 +24,42 @@ const blurryColors = {
   `,
 }
 
-const blurryEffectByBrowser: any = {
-  safari: css`
-    filter: blur(120px);
-    animation-name: none;
-    height: 100%;
-  `,
-  ios: css`
-    filter: blur(120px);
-    animation-name: none;
-    height: 100%;
-  `,
-  default: css`
-    @keyframes blurry {
-      from {
-        background-position: 0vh 0vh;
-      }
-
-      to {
-        background-position: 100vw 100vh;
-      }
+const blurEffect = css`
+  @keyframes blurry {
+    from {
+      background-position: -100vw -130vh;
+      filter: hue-rotate(0deg);
     }
-    animation: blurry 8s reverse cubic-bezier(0.36, 0, 0.66, -0.56);
-  `,
-}
 
-const blurEffect = browser?.name
-  ? blurryEffectByBrowser[browser.name] || blurryEffectByBrowser.default
-  : blurryEffectByBrowser.default
+    to {
+      background-position: 0vw 0vh;
+      filter: hue-rotate(360deg);
+    }
+  }
+  animation: blurry 8s reverse cubic-bezier(0.36, 0, 0.66, -0.56);
+`
 
 export const Blurry = styled.div<{ color: keyof typeof blurryColors }>`
-  background-image: linear-gradient(
-    to bottom right,
-    #ddd 40%,
-    transparent,
-    #444 40%
-  );
-  filter: blur(300px);
   position: absolute;
   width: 100%;
-  height: 110%;
+  height: 120%;
   top: 0;
   left: 0;
-  background-position: 0px 0px;
+  background-position: -100vw -130vh;
+  background-size: 400% 400%;
+  background-repeat: no-repeat;
   animation: blurry 8s reverse cubic-bezier(0.36, 0, 0.66, -0.56);
+  filter: hue-rotate(0deg);
+
+  &:after {
+    content: " ";
+    position: absolute;
+    width: 100%;
+    height: 150%;
+    bottom: 0;
+    left: 0;
+    background-image: linear-gradient(to bottom, transparent, rgb(14 12 11/1));
+  }
 
   @media (prefers-reduced-motion) {
     animation-name: none;
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,6 @@
-declare const __PATH_PREFIX__: string;
+declare const __PATH_PREFIX__: string
+
+declare module "*.png" {
+  const url: string
+  export default url
+}
