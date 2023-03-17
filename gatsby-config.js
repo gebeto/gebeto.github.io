e6aebc5febf6d4aa40ca3bb7006512c127e5330a
gebeto <slavik.nychkalo@gmail.com>
2023-03-17 12:42:33 +0200

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -136,15 +136,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `gebeto portfolio`,
+        short_name: `gebeto`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
